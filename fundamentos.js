568e1503851e804ce9ea5fda4b175b1867427f78
Yarlley Fernanes <yarlleyfernandes9@gmail.com>
2023-11-16 22:05:09 -0300

    concluído os fundamentos

--- fundamentos.js
+++ fundamentos.js
@@ -134,3 +134,107 @@ do {
     console.log('O valor de j é: ' + j)
     j++
 } while(j < 5);
+
+// Funções (serve para salva códigos que queira usar depois)
+// function nome (arg1, arg2) { corpo }
+function cumprimentar(nome) {
+    console.log('Olá ' + nome)
+}
+
+// invocação == nome()
+cumprimentar('Yarlley');
+
+// escopo de variáveis
+let cor = 'azul'; // fora de chaves escopo global
+
+function mostrarCor() {
+    let cor = 'verde' // dentro de chaves escopo local
+
+    console.log(cor)
+}
+
+console.log(cor)
+
+mostrarCor()
+
+// hoisting == içamento
+testeHoisting()
+
+function testeHoisting() {
+    console.log('Deu certo!')
+}
+
+// arrow function
+const testeArrow = () => console.log('Isso também é uma função')
+
+testeArrow();
+
+// truthy e falsy
+const minhaVarivel1 = '' // falsy
+const minhaVarivel2 = 'algum texto' // truthy
+
+if(minhaVarivel1) {
+    console.log('É verdadeiro!')
+} else {
+    console.log('É falso!')
+}
+
+if(minhaVarivel2) {
+    console.log('É verdadeiro!');
+} else {
+    console.log('É falso!');
+}
+
+// array, listas
+const numeros = [1, 2, 3, 4, 5];
+
+console.log(numeros);
+
+console.log(numeros[0]);
+
+console.log(numeros[2]);
+
+numeros.push(6, 7)
+
+console.log(numeros)
+
+//prototype => OBJETO -> objeto2
+// Array => nossos arrays
+
+numeros.pop() // exclui o último número
+
+console.log(numeros)
+
+// strings
+const minhaStringNova = 'Olá, Mundo!'
+
+//concatenação == +
+const minhaString3 = minhaStringNova + ' Como você está?'
+
+console.log(minhaString3)
+
+// interpolação
+const minhaString4 = `${minhaStringNova} Como você esta?`
+console.log(minhaString4)
+
+console.log(minhaString4.length) // quantidade de caracteres
+console.log(minhaString4[5]) // mostra a caractere de acordo com o índice
+console.log(minhaString4.toUpperCase())
+
+// Data e hora
+const agora = Date()
+console.log(agora)
+
+const natal = new Date(2023, 11, 25)
+
+console.log(natal)
+
+// Math
+console.log(Math.PI)
+
+console.log(Math.round(3.4))
+
+console.log(Math.sqrt(16))
+
+console.log(Math.pow(2, 3))
+
